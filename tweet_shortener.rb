d116037d170a10c8c3bb0676b40af84292d6b326
Pascal Rassaby <pasbynumbers@gmail.com>
2019-05-10 19:18:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,24 @@
 # Write your code here.
-def tweet_shortener
-  
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
 end
+
+def word_substituter(string) 
+  string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word 
+    end 
+  end.join(" ")
+end 
